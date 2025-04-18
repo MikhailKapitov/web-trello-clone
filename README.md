d5cc40138816c86553820d958c9a6d55795e1943
MikhailKapitov <mkapitov4@gmail.com>
2025-04-18 11:59:11 +0500

    Initiated a simple empty project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple Trello-like project
+
+WIP
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+
+export default function App() {
+
+  return (
+    <>
+      <header className="header">
+        WIP
+      </header>
+
+      <content id="content">
+        WIP
+      </content>
+
+      {}
+      <footer className="footer">
+        WIP
+        <br></br>
+        Lorem ipsum dolor sit amet!
+      </footer>
+    </>
+  );
+}
